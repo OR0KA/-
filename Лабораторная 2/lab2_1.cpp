1a80d2f44a44f502d4e4d19d36cc9e2316b72d96
OR0KA <55181912+OR0KA@users.noreply.github.com>
2019-09-18 18:36:07 +0300

    Update lab2_1.cpp

--- Лабораторная 2/lab2_1.cpp
+++ Лабораторная 2/lab2_1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 	int m[4], d;
-	//for (int i = 0; i < 4; i++) cin >> m[i];
+	for (int i = 0; i < 4; i++) cin >> m[i];
 	d = sqrt(pow(m[2] - m[0], 2) + pow(m[3] - m[1], 2));
 	cout << d;
 }
