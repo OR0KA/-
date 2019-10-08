67fc1c4742d65951f7928ac26eecaa592b10afff
OR0KA <55181912+OR0KA@users.noreply.github.com>
2019-10-08 22:42:42 +0300

    кшгрпРПШЫ

---
+++ Лабораторная 8/lab8_2.cpp
@@ -0,0 +1,36 @@
+﻿// 2. Даны три числа. Найти сумму двух наибольших из них.
+//
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a, b, c, S;
+	cin >> a >> b >> c;
+	if ((a <= b) & (a <= c))
+	{
+		S = b + c;
+		cout << S;
+	}
+	else
+	{
+		if ((b <= a) & (b <= c))
+		{
+			S = a + c;
+			cout << S;
+		}
+		else
+		{
+			if ((c <= a) & (c <= b))
+			{
+				S = a + b;
+				cout << S;
+			}
+		}
+	}
+	
+}
---
+++ Лабораторная 8/lab8_3.cpp
@@ -0,0 +1,38 @@
+﻿// 3. На плоскости расположены три точки: A, B, C. Определить, 
+// какая из двух последних точек (B или C) расположена ближе к A, и вывести эту точку и ее расстояние от точки A.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	double x1, y1, x2, y2, x3, y3, b, c;
+
+	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+	b = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+	c = sqrt(pow((x3 - x1), 2) + pow((y3 - y1), 2));
+
+	if (b < c)
+	{
+		cout <<"b ближе" << endl << "AB= " << b;
+	}
+	else
+	{
+		if (c < b)
+		{
+			cout << "c ближе" << endl << "AC= " << c;
+		}
+		else {
+			cout << "Расстояния равны";
+		}
+	}
+
+}
+
+
+
+
