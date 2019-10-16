d12382940e367725733638bfb019953b6e5f109d
OR0KA <55181912+OR0KA@users.noreply.github.com>
2019-10-16 12:05:59 +0300

    Здесь могла быть ваша реклама

---
+++ Лабораторная 11/lab11_5.cpp
@@ -0,0 +1,22 @@
+﻿//5. Даны целые положительные числа A и B. Найти их наибольший общий делитель (НОД), используя алгоритм Евклида
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a, b;
+	cout << "Введите a и b: " << endl;
+	cin >> a >> b;
+	while (a != b)
+	{
+		if (a > b) a = a - b;
+		else b = b - a;
+	}
+	cout << "НОД: " << a;
+}
+
+
---
+++ Лабораторная 11/lab11_6.cpp
@@ -0,0 +1,25 @@
+﻿// 6. Дано целое число N (> 1), являющееся числом Фибоначчи: N = FK.
+// Найти целое число K — порядковый номер числа Фибоначчи N.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, k = 2, fib = 0, fib1 = 1, fib2 = 1;
+	cout << "Введите n: ";
+	cin >> n;
+	while (n > fib)
+	{
+		fib = fib1 + fib2;
+		k += 1;
+		if (n == fib) cout << "Порядковый номер заданного числа Фибоначчи - " << k;
+		fib2 = fib1;
+		fib1 = fib;
+	}
+	if (n < fib) cout << "Заданное число - не число Фибоначчи";
+
+}
