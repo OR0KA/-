08158ef5e81b49daf226b22d20b5f3d070d450b4
OR0KA <55181912+OR0KA@users.noreply.github.com>
2019-10-29 22:34:11 +0300

    ya uje ustala pridumivat, cho suda pisat

---
+++ Лабораторная 12/lab12_1.cpp
@@ -0,0 +1,32 @@
+﻿// 1. Описать функцию PowerA3(A, B), вычисляющую третью степень числа A 
+// и возвращающую ее в переменной B (A — входной, B — выходной параметр; оба параметра являются вещественными).
+// С помощью этой функции найти третьи степени пяти данных чисел.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void PowerA3(double A, double& B)
+{
+	B = A * A * A;
+}
+
+int main(void)
+{
+	int i;
+	double mas[5];
+	for (i = 0; i < 5; i++)
+	{
+		cin >> mas[i]; 
+	}
+	for (i = 0; i < 5; i++)
+	{
+		PowerA3(mas[i], mas[i]);
+		cout << mas[i] << ' ';
+	}
+}
+
+
+
+
---
+++ Лабораторная 12/lab12_2.cpp
@@ -0,0 +1,26 @@
+﻿// 2. Описать функцию Sign(X) целого типа, возвращающую для вещественного числа X следующие значения :
+// −1, если X < 0; 0, если X = 0; 1, если X > 0.
+// С помощью этой функции найти значение выражения Sign(A) + Sign(B) для данных вещественных чисел A и B.
+
+#include <iostream>
+
+using namespace std;
+
+int Sign(double X)
+{
+	if (X < 0) return -1;
+	else if (X == 0) return 0;
+	else return 1;
+}
+int main()
+{
+	double a, b, c;
+	cin >> a >> b;
+	c = Sign(a) + Sign(b);
+	cout << c;
+
+}
+
+
+
+
