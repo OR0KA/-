284ba69ca324ad2cb544f8eee2efb8a8d3e3180d
OR0KA <55181912+OR0KA@users.noreply.github.com>
2019-11-19 01:59:57 +0300

    надо радоваться
    не надо напрягаться

---
+++ Лабораторная 14/lab14_1.cpp
@@ -0,0 +1,27 @@
+﻿// 1. Дан массив размера N и целые числа K и L (1 ≤ K ≤ L ≤ N).
+// Найти среднее арифметическое элементов массива с номерами от K до L включительно.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int N, K, L, M=1, F, v = 0;
+	cout << "Введите N "; cin >> N;
+	cout << "Введите L (<=N) "; cin >> L;
+	cout << "Введите K (1<=K<=L) "; cin >> K;
+	
+	int* g = new int[N];
+	for (int i = K; i <= L; i++)
+	{
+		g[i] = i;
+		M += g[i];
+		v++;
+	}
+	 
+	F = M / v;
+	cout << F;
+}
---
+++ Лабораторная 14/lab14_3.cpp
@@ -0,0 +1,31 @@
+﻿// 3. Дан массив A размера N. Найти минимальный элемент из его элементов с четными номерами: A2, A4, A6...
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int N, i;
+	int k = 2147483647;
+
+	cin >> N;
+
+	int* A = new int[N];
+
+	for ( i = 0; i < N; i++) cin >> A[i];
+
+	for (i = 1; i < N; i+=2)
+	{
+		if (k > A[i]) k = A[i];
+	}
+
+	cout << k;
+}
+
+
+
+
